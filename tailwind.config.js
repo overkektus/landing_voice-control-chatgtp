eeb44732d0290c0606a9ab3f4f0262da3b388999
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-15 13:44:27 +0200

    apply all sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#88EF96',
+        secondary: '#69BBFF',
+        accent: '#FFA048',
+        gray: '#3D4A3E',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +21,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
